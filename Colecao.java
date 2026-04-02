26c07d001647833da8852157a2788c451dabf001
3nr19u3C0sta <enrique-carvalho@hotmail.com>
2026-04-02 00:13:04 -0300

    Nova area

--- Colecao.java
+++ Colecao.java
@@ -1,3 +1,10 @@
+import  java.util.HashList;
 public class Colecao {
-
+    public int[] twoSum(int[] nums, int target) {
+        Hash<Integer, Integer> lista = new HashList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complemento = target - nums[i];
+            if(lista.contains)
+        }
+    }
 }
